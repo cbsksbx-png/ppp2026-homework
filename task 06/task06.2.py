ac0cfea69b65c4153b1896db57d9b64693fdef6f
cbsksbx <cbsksbx@gmail.com>
2026-03-29 05:59:22 +0900

    Add files via upload

---
+++ task 06/task06.2.py
@@ -0,0 +1,3 @@
+star = int(input("개수를 입력하시오"))
+for m in range(1 , star + 1) :
+    print("*" * m)
---
+++ task 06/task06.4.py
@@ -0,0 +1,10 @@
+from cmath import cos
+from math import degrees, radians
+
+import math
+for i in range(0 , 90 + 1) :
+    rad = math.radians(i)
+    sin = round(math.sin(rad), 4)
+    cos = round(math.cos(rad), 4)
+    tan = round(math.tan(rad), 4)
+    print(round(rad, 4), sin, cos, tan)
