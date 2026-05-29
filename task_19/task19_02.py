9b7b0ef5d6ec1ffcdb82e23acda5e132c9b68745
cbsksbx-png <cbsksbx@gmail.com>
2026-05-30 02:50:12 +0900

    day05_30

--- task_19/task19_02.py
+++ task_19/task19_02.py
@@ -10,17 +10,20 @@ def gugudan_game():
     return
 
 def main():
-    life = 3
     while True:
         score = 0
+        life = 3
         for i in range(10):
             if gugudan_correct():
                 score += 10
-            if not gugudan_correct():
+            else:
                 life -= 1
                 print(f"틀리셨습니다. 남은 목숨은{life}개 입니다.")
-            elif life == 0:
-                print(f"게임이 종료되었습니다. 총 점수는 {score}입니다.")
-    print(f"총 점수는 {score}입니다.")
+                if life == 0:
+                    print(f"게임 종류입니다. 점수는 {score}입니다.")
+                    break
+        print(f"총 점수는 {score}입니다.")
+        break
+
 if __name__ == '__main__':
     main()
