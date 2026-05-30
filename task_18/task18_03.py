18951f21f0ac6c9d2000431a6e0740969deaad81
cbsksbx-png <cbsksbx@gmail.com>
2026-05-30 17:23:05 +0900

    day05_30

--- task_18/task18_03.py
+++ task_18/task18_03.py
@@ -11,21 +11,23 @@ def korean_to_be_initial(ran_word):
             r_list.append([w])
     return r_list
 
-def word_list(word):
-    ran_word = random.choice(word)
-    return korean_to_be_initial(ran_word), ran_word
+# def word_list(word):
+#     ran_word = random.choice(word)
+#     return korean_to_be_initial(ran_word), ran_word
 
 
 def main():
     word = [ "바나나", "사과", "귤" ]
-    word_initial = word_list(word)[0]
-    r_word = word_list(word)[1]
-    print(word_initial)
-    print(r_word)
+    random_word = random.choice(word)
+    ran_word_initial = korean_to_be_initial(random_word)
+    # word_initial = word_list(word)[0]
+    # r_word = word_list(word)[1]
+    print(ran_word_initial)
+    # print(r_word)
     # -> r_word 랑 word_initial 이랑 달라서 문제 발생
     while True:
         x = input("X=> ?")
-        if x == r_word:
+        if x == random_word:
             print("정답입니다.")
             break
         else:
